53378d264f84779d353c8a511fd47b1ad91ffb1f
sabrinaDz <sabrinasahli@live.fr>
2021-09-08 01:49:49 -0700

    fix patient loader #11

--- src/main/java/com/sihatech/mspatient/mspatient/web/bootstrap/PatientLoader.java
+++ src/main/java/com/sihatech/mspatient/mspatient/web/bootstrap/PatientLoader.java
@@ -16,11 +16,11 @@ import java.util.UUID;
 @Component
 public class PatientLoader implements CommandLineRunner {
 
-    public static final UUID PATIENT1_UUID= UUID.fromString("d8888e11-c667-42da-b0d5-8e28e05670bb");
+   /* public static final UUID PATIENT1_UUID= UUID.fromString("d8888e11-c667-42da-b0d5-8e28e05670bb");
     public static final UUID PATIENT2_UUID= UUID.fromString("4869a849-cc02-4468-9e29-d9acb666ad2e");
     public static final UUID PATIENT3_UUID= UUID.fromString("7788068f-8854-43c8-b5cc-93866664bb80");
     public static final UUID PATIENT4_UUID= UUID.fromString("c45994ac-2974-4cc9-a13f-7c6ae75a5d13");
-    public static final UUID PATIENT5_UUID= UUID.fromString("16b7b37c-5372-4a23-95f8-0e1c4b3095f3");
+    public static final UUID PATIENT5_UUID= UUID.fromString("16b7b37c-5372-4a23-95f8-0e1c4b3095f3");*/
 
     private final PatientRepository patientRepository;
 
@@ -36,7 +36,8 @@ public class PatientLoader implements CommandLineRunner {
     }
 
     private void loadPatientsObjects() {
-        Patient patient1=Patient.builder().id(PATIENT1_UUID)
+        Patient patient1=Patient.builder()
+                //.id(PATIENT1_UUID)
                 .firstName("sabrina2")
                 .lastName("sahli2")
                 .dateOfBirth(LocalDate.of(1988, Month.FEBRUARY,2))
@@ -45,7 +46,8 @@ public class PatientLoader implements CommandLineRunner {
                 .size(1.61)
                 .build();
 
-        Patient patient2=Patient.builder().id(PATIENT2_UUID)
+        Patient patient2=Patient.builder()
+                //.id(PATIENT2_UUID)
                 .firstName("sabrina2")
                 .lastName("sahli2")
                 .dateOfBirth(LocalDate.of(1988, Month.FEBRUARY,2))
@@ -54,7 +56,8 @@ public class PatientLoader implements CommandLineRunner {
                 .size(1.61)
                 .build();
 
-        Patient patient3=Patient.builder().id(PATIENT3_UUID)
+        Patient patient3=Patient.builder()
+                //.id(PATIENT3_UUID)
                 .firstName("sabrina2")
                 .lastName("sahli2")
                 .dateOfBirth(LocalDate.of(1988, Month.FEBRUARY,2))
@@ -63,7 +66,8 @@ public class PatientLoader implements CommandLineRunner {
                 .size(1.61)
                 .build();
 
-        Patient patient4=Patient.builder().id(PATIENT4_UUID)
+        Patient patient4=Patient.builder()
+                //.id(PATIENT4_UUID)
                 .firstName("sabrina2")
                 .lastName("sahli2")
                 .dateOfBirth(LocalDate.of(1988, Month.FEBRUARY,2))
